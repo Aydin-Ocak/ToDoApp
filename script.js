e3f227d67035bde44465708ca5b7356d6d860451
Aydın <aydioncakft@gmail.com>
2024-09-29 01:10:40 +0300

    Commit mesajı

---
+++ script.js
@@ -0,0 +1,80 @@
+let taskName = document.getElementById("txtName");
+let addBtn = document.querySelector("#btnAdd");
+let deleteAllBtn = document.querySelector("#btnDeleteAll");
+
+let listObject = [];
+let objectList = document.querySelector("#objectList");
+
+
+
+function getList(event) {
+    objectList.innerHTML = "";
+
+    for (let i in listObject) {
+        let object = `
+        <div id="${i}" class="object">
+            <label class="lbl">${listObject[i].taskName}</label>
+            <div class="buttons">
+                <button class="btnObj" id="btnFvr">
+                    <i class="fa-regular fa-bookmark"></i>
+                </button>
+                <button class="btnObj" id="btnDelete">
+                    <i class="fa-solid fa-xmark"></i>
+                </button>
+            </div>
+        </div>
+    `;
+        objectList.insertAdjacentHTML("beforeend", object);
+
+        if(listObject[i].fvrt == 1){
+            document.getElementById(i).style.backgroundColor = "#fca311";
+            }
+    }
+
+    document.querySelectorAll("#btnDelete").forEach(b => {
+        b.addEventListener("click", deleteObject);
+    })
+    
+    document.querySelectorAll("#btnFvr").forEach(b => {
+        b.addEventListener("click", fvrObject);
+    })
+}
+
+var addObject = () => {
+    if (taskName.value != "") {
+        let count = listObject.length;
+
+        listObject.push({ id: count + 1, taskName: taskName.value, fvrt:0 })
+        getList();
+    } else {
+        alert("Please Add Task Name!")
+    }
+
+}
+
+var deleteAll = () => {
+    listObject = [];
+    getList();
+}
+
+var deleteObject = (e) => {
+    let ids = e.currentTarget.parentElement.parentElement;
+    delete listObject[ids.id];
+    getList();
+    console.log(ids)
+}
+var fvrObject = (e)=>{
+    let ids = e.currentTarget.parentElement.parentElement;
+    if(listObject[ids.id].fvrt == 0){
+        listObject[ids.id].fvrt=1;
+    }else if(listObject[ids.id].fvrt ==1){
+        listObject[ids.id].fvrt =0;
+    }
+
+    getList();
+}
+
+getList();
+
+addBtn.addEventListener("click", addObject)
+deleteAllBtn.addEventListener("click", deleteAll);
